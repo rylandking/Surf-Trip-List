f7ef2d78da5eceb4fca90f36c942fc8d694b83f6
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-6.local>
2018-04-29 15:35:43 -0700

    Show specific city info on location page 50% done

--- script.js
+++ script.js
@@ -208,7 +208,7 @@ db.collection("city").get().then(function(querySnapshot) {
         //add info to LOCATION CARD
         //$ is the same as getElementById
         $("#card-container").prepend(
-          `<a id="city-card" data-id="${cityName}" href="location-page.html"><div class="card bg-dark text-white ${expFilter}">
+          `<a id="city-card" data-id="${cityName}"><div class="card bg-dark text-white ${expFilter}">
             <img class="img-fluid card-img" src="${cityimage}">
               <div class="card-img-overlay">
 
@@ -227,23 +227,27 @@ db.collection("city").get().then(function(querySnapshot) {
 });// END - Loop the city collection. Populate location cards
 
 
-//Click on city card takes you to location page for ONLY that city
-$('body').on('click','#city-card',function(e){
-    e.preventDefault();
+//START_DRAFT Click on city card takes you to location page for ONLY that city
+$(document).ready(function(){
+  $('body').on('click','#city-card',function(e){
+    // e.preventDefault();
     //Stores data-id=${cityName} in variable cityPage
-    const cityPage = $(this).data('id');
-    // console.log(cityPage);
+    var cityPage = $(this).data('id');
+    console.log(cityPage);
 
-    const docRef = db.collection("city").doc(cityPage);
+    const docRef = db.collection("city").doc(cityPage)
     docRef.get().then(function(doc) {
       var citydata = doc.data();
       var cityName = doc.id;
       console.log("cityName: " + cityName);
 
+    window.location.href = "location-page.html";
+
+      });
     });
-});
-//^^^NEXT -- Only get data where city = cityPage. Show that on location-page
-//^^^NEXT -- Populate related variables to cityPage
+  });
+  //END_DRAFT Click on city card takes you to location page for ONLY that city
+
 
 // START - Loop the city collection. Populate the guide on the location page
 db.collection("city").get().then(function(querySnapshot) {
@@ -858,7 +862,7 @@ db.collection("city").get().then(function(querySnapshot) {
         }
 
           //Initialize the AccommMap
-          initAccommMap()
+          initAccommMap();
 
           //AccommMap options
           var options = {
