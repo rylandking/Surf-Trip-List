d48e2174f3cbe74fc38d8ffc17280086171a92ab
MacBook Pro <macbookpro@MacBooks-MacBook-Pro-6.local>
2018-07-05 13:26:54 -0400

    75% smaller images and name, website, phone, address, review and rating from GP JS Library

--- script.js
+++ script.js
@@ -1392,6 +1392,58 @@ docRef.get().then(function(doc) {
 
 });//END OF POPULATING BOARD RENTAL MAP
 
+////START DRAFT Surfboard Rentals API search
+
+// var map;
+// var service;
+// var infowindow;
+//
+// function initialize() {
+//   var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+//
+//   map = new google.maps.Map(document.getElementById('map'), {
+//       center: pyrmont,
+//       zoom: 15
+//     });
+//
+//   var request = {
+//     location: pyrmont,
+//     radius: '500',
+//     query: 'restaurant'
+//   };
+//
+//   service = new google.maps.places.PlacesService(map);
+//   service.textSearch(request, callback);
+// }
+//
+// function callback(results, status) {
+//   if (status == google.maps.places.PlacesServiceStatus.OK) {
+//     for (var i = 0; i < results.length; i++) {
+//       var place = results[i];
+//       console.log(place)
+//       createMarker(results[i]);
+//     }
+//   }
+// }
+
+//DRAFT AJAX CALL OF GOOGLE PLACES API (SERVER-SIDE)
+$(function sbRentalsSearch(){
+  var sbRentalsSearchURL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=surfboard+rentals+santa+barbara+california&key=AIzaSyAXPJDWXhzlAY0N8mMUcgFJ6yNSEd0vMg8`;
+  $.ajax({
+    type: 'GET',
+    url: `${sbRentalsSearchURL}`,
+    success: function(response) {
+      console.log('success', response);
+    } //END sbRentalsSearch success function
+  }); //End sbRentalsSearch AJAX call
+}); //END sbRentalsSearch funciton
+
+
+// `https://maps.googleapis.com/maps/api/place/textsearch/json?query=surfboard+rentals+santa+barbara+california&key=AIzaSyAXPJDWXhzlAY0N8mMUcgFJ6yNSEd0vMg8`;
+//`https://maps.googleapis.com/maps/api/js?key=AIzaSyAXPJDWXhzlAY0N8mMUcgFJ6yNSEd0vMg8&libraries=places&query=surfboard+rentals+santa+barbara+california`;
+
+////END DRAFT Surfboard Rentals API search
+
 //Populates contents of Rentals section
 window.initBoardRentalMap = function () {
   $("#board-rentals__wrapper").prepend(`
